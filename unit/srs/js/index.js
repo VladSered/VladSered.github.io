07f48e7e6689f3b05c71388c5323a9c5e911e1d9
Vladislav <vladislav08011993@outlook.com>
2018-01-23 22:17:22 +0300

    array

--- unit/srs/js/index.js
+++ unit/srs/js/index.js
@@ -119,7 +119,7 @@ runGame();
 5.
 Функция printResult должна принять номер игрока, который выиграл и напечатать в div Id result текстовое сообщение типа: «выиграл первый игрок» номер игрока надо вывести словами.
 */
-
+/*
 var btn = document.getElementById("play");
 var player1 = document.getElementById("player1");
 var player2 = document.getElementById("player2");
@@ -186,3 +186,170 @@ function runGame() {
 
 }
 btn.addEventListener("click", runGame);
+*/
+
+
+
+/*
+Домашнее задание:
+Цель: потренироваться в работе с массивами и объектами.
+
+ЧИТАТЬ ВНИМАТЕЛЬНО, МНОГО ДЕТАЛЕЙ!
+
+Вам необходимо из массива данных, который находится в файле data.js создать новый выполнив серию преобразований.
+Новый массив объектов необходимо вывести в консоль. Функционал для этого уже есть. Скачайте заготовку и нажмите на кнопку.
+Затем откройте консоль.
+Файл data.js уже подключен, по этому просто пишите код вашей программы, массив должен быть доступен.
+
+Внимание: если какое то преобразование сделать не удается. Пропускаете его и и двигайтесь дальше.
+
+1.
+Задайте переменную в которой укажите сколько элементов вы будете обрабатывать.
+В оригинальном массиве 15 элементов. В вашей переменной должно быть число которое показывает сколько элементов будет в новом массиве. Остальные отсекаются.
+То есть допустим было 15 а в результирующем будет 7. Остальные отбрасываем.
+
+2.
+Используем функцию forEach.
+Внутри forEach задаем условие таким образом, чтоб выбирать то количество элементов, которое указано в переменной что мы задали выше.
+То есть создаем новый массив объектов с нужным количеством. Остальное отбрасываем.
+Кроме того в процессе создания нового массива объектов, избавляемся то ключа id.
+То есть в вашем новом массиве не должно быть id в конкретных объектах.
+Подсказка:
+
+data.forEach(function(item, index){
+    var newArr = [];
+    if(count<=index){
+        newArr.push({
+            name: item.name
+            ////.....
+        })
+    }
+})
+3.
+По новому массиву объектов, полученному с помощю функции forEach проходимся с помощю map
+
+4.
+На каждой итерации цикла мы получаем один объект из массива объектов. Берем этот объект и преобразовываем его поля по следующим правилам.
+Вам пригодится документация по дате и по стрингам.
+
+5.
+Для поля Name: с помощью функций работы со стрингами делаете первую букву большой, остальные маленькие (ДЖИП -> Джип)
+
+6.
+Для поля url: добавить перед ним «http://»
+
+7.
+Для поля Description: с помощью функций работы со стрингами делаете обрезание до 15 символов. После добавляем многоточие (…) Остальное отбрасывете.
+
+8.
+Для поля date: создать объект даты из заданных милисекунд и потом отобразить в виде «2015/07/02 14:15»
+Для этого надо открыть документацию по дате. Ссылка есть в конце презентации.
+Пример как преобразовывать дату. Можно использовать и другой подход. Главное чтоб он не был более громоздким и сложным.
+
+var date = 1422153200637;
+var newDate = function(date){
+    var tmpDate = new Date(date);
+    return tmpDate.getFullYear() + "/" +
+           tmpDate.getMonth() + "/" +
+           tmpDate.getDate() + " " +
+           tmpDate.getHours() + ":" +
+           tmpDate.getMinutes();
+};
+9.
+Для поля params: из значений ключей сформировать стрингу типа «true=>80». Для выполнения задания можно обращаться к полям объект params напрямую.
+То есть params.status и params.progress
+
+10.
+После всех преобразований функция map вернет вам новый массив, который вы в результате печатаете. Для печати используем отдельную функцию как в прошлых заданиях. То есть вынесете console.log в отдельную функцию.
+
+11.
+Пример результата (количество элементов в результате должно быть не два а сколько укажете 
+    в переменной):
+
+var data = [{
+	url: "http://desktopwallpapers.org.ua/mini/201507/40069.jpg",
+	name: "CHEVROLET",
+	params : "true=>80",
+	description : "be conveyed to ...",
+	date : "2015/01/25 14:15"
+},{
+	url: "http://desktopwallpapers.org.ua/mini/201507/40068.jpg",
+	name: "DEWOO",
+	params : "true=>88",
+	description : "sing color to a...",
+	date : 2015/12/18 15:35"
+}]
+*/
+/*
+var btn = document.getElementById("play");
+// Создание массива
+function createArr(data) {
+    var count = 5;
+    var array = [];
+    data.forEach(function(item, index){
+        if(count > index){
+            array.push({
+                url: item.url,
+                name: item.name,
+                params: item.params,
+                description: item.description,
+                date: item.date
+            });
+        }
+    });
+    return array;   
+}
+// Изменение регистра
+function editName(name) {
+    return name[0] + name.substring(1).toLowerCase();    
+}
+// Добавление "http://"
+function editUrl(url) {
+    return "http://"+url;    
+}
+
+// Обрезаем до 15 символов и добавляем (...)
+function editDescription(description) {
+    return description.slice(0, 15) + "...";
+}
+// Настройка отображения времени 
+function editData(date) {
+        var tmpDate = new Date(date);
+        return tmpDate.getFullYear() + "/" +
+               tmpDate.getMonth() + "/" +
+               tmpDate.getDate() + " " +
+               tmpDate.getHours() + ":" +
+               tmpDate.getMinutes();
+}
+// Сформировать стрингу типа «true=>80»
+function editParams(params) {
+    return params.status + "=>" + params.progress;
+}
+// Работа с массивом через map
+function editArr(arr) {
+    var newArr =  arr.map(function(item, index){
+        return {
+            name: editName(item.name),
+            url: editUrl(item.url),
+            description: editDescription(item.description),
+            date: editData(item.date),
+            params: editParams(item.params)    
+        }    
+    });
+    return newArr;  
+}
+
+// Вывод массива в консоле 
+function print(someText) {
+    console.log(someText);
+}
+
+function transform() {
+    var array = createArr(data);
+    var result = editArr(array);
+    print(result);
+}
+
+btn.addEventListener("click", transform);
+*/
+
