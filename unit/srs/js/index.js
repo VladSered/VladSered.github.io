fd39ae55ebc573ecdc532321a8901f592379b468
Vladislav <vladislav08011993@outlook.com>
2018-03-16 21:37:20 +0300

    first task

--- unit/srs/js/index.js
+++ unit/srs/js/index.js
@@ -2,15 +2,25 @@ var json;
     // обработчик события загрузки страницы.
     window.onload = function () {
         // Создание объекта запроса.
-        $("#action").click(function(){
+        $('#action').click(function(){
             $.ajax({
-                type: "GET",
-                url: "https://jsonplaceholder.typicode.com/users",
-                dataType: "json",
+                type: 'GET',
+                url: 'https://jsonplaceholder.typicode.com/users',
+                dataType: 'json',
 
                 // функция получает десериализованный объект
                 success: function (obj) {
-                    console.log(obj);
+                    var data = '';
+                    $.each(obj, function (key, value) {
+                        // генерируем HTML
+                        data += '<div>';
+                        data += '<p>' + value.id + '</p>';
+                        data += '<p>' + value.name + '</p>';
+                        data += '</div>';
+                        // data += '<p>' + value.id + '</p>';
+                    });
+                    //отправляем данные на страницу
+                    $('#out').append(data);
                 }
             });
         });
