9df584f2551a1c0dfc2b30970088462890f1cdab
Vladislav <vladislav08011993@outlook.com>
2018-03-19 13:39:42 +0300

    win

--- unit/srs/js/index.js
+++ unit/srs/js/index.js
@@ -1,26 +1,28 @@
-var json;
-    // обработчик события загрузки страницы.
-    window.onload = function () {
-        // Создание объекта запроса.
-        $('#action').click(function(){
-            $.ajax({
-                type: 'GET',
-                url: 'https://jsonplaceholder.typicode.com/users',
-                dataType: 'json',
+// обработчик события загрузки страницы.
+window.onload = function () {
+    // Создание объекта запроса.
+    $('#action').click(function () {
+        $.ajax({
+            type: 'GET',
+            url: 'https://jsonplaceholder.typicode.com/users',
+            dataType: 'json',
 
-                // функция получает десериализованный объект
-                success: function (obj) {
-                    var data = '';
-                    $.each(obj, function (key, value) {
-                        // генерируем HTML
-                        data += '<div>';
-                        data += '<p>' + value.id + '</p>';
-                        data += '<p>' + value.name + '</p>';
-                        data += '</div>';
-                    });
-                    // отправляем данные на страницу
-                    $('#out').append(data);
-                }
-            });
+            // функция получает десериализованный объект
+            success: function (obj) {
+                $.each(obj, function (key, value) {
+                    $('#out').append('<ul class="list">\
+                        <li class="name">'+ value.name +'</li>\
+                        <li class="email">' + '<span><i class="fa fa-envelope"></i> </span>' + value.email +'</li>\
+                        <li class="phone">' + '<span><i class="fa fa-mobile"></i> </span>' + value.phone +'</li>\
+                        <li class="adress">' + '<span>Adress: </span>' + '<ul>\
+                            <li class="place">' + '<span>street: </span>' + value.address.street + '</li>\
+                            <li class="place">' + '<span>suite: </span>' + value.address.suite + '</li>\
+                            <li class="place">' + '<span>city: </span>' + value.address.city + '</li>\
+                            </ul>'+'</li>\
+                        <li class="company">' + '<span>Company: </span>' + value.company.name +'</li>\
+                    </ul>');
+                });
+            }
         });
-    }
+    });
+}
