365b359231e38afc5bbfc9cc34e6720ce413a7f8
Vladislav <vladislav08011993@outlook.com>
2018-03-23 08:51:47 +0300

    ok

--- unit/srs/js/index.js
+++ unit/srs/js/index.js
@@ -16,15 +16,15 @@ $(document).ready(function(){
             success: function (obj) {
                 $.each(obj, function (key, value) {
                     $('#out').append('<ul class="list"><button class="close_card"><i class="fas fa-times"></i></button>\
-                        <li class="name edit">' + '<span><i class="fas fa-user"></i> </span>' + value.name +'</li>\
-                        <li class="email edit">' + '<span><i class="fa fa-envelope"></i> </span>' + value.email +'</li>\
-                        <li class="phone edit">' + '<span><i class="fa fa-mobile"></i> </span>' + value.phone +'</li>\
+                        <li class="name">' + '<span><i class="fas fa-user"></i> </span>' + '<span class="edit">'+ value.name +'</span></li>\
+                        <li class="email">' + '<span><i class="fa fa-envelope"></i> </span>' + '<span class="edit">'+ value.email +'</span></li>\
+                        <li class="phone">' + '<span><i class="fa fa-mobile"></i> </span>' + '<span class="edit">'+ value.phone +'</span></li>\
                         <li class="adress">' + '<span>Adress: </span>' + '<ul>\
-                            <li class="place edit">' + '<span>street: </span>' + value.address.street + '</li>\
-                            <li class="place edit">' + '<span>suite: </span>' + value.address.suite + '</li>\
-                            <li class="place edit">' + '<span>city: </span>' + value.address.city + '</li>\
+                            <li class="place">' + '<span>street: </span>' + '<span class="edit">'+ value.address.street +'</span></li>\
+                            <li class="place">' + '<span>suite: </span>' + '<span class="edit">'+ value.address.suite +'</span></li>\
+                            <li class="place">' + '<span>city: </span>' + '<span class="edit">'+ value.address.street +'</span></li>\
                             </ul>'+'</li>\
-                        <li class="company edit">' + '<span>Company: </span>' + value.company.name +'</li>\
+                        <li class="company">' + '<span>Company: </span>' + '<span class="edit">'+ value.company.name +'</span></li>\
                     </ul>');
                 });
                 //удаляем кнопку из DOM
@@ -42,7 +42,7 @@ $(document).ready(function(){
             var text =  $(this).text(); 
 
             $(this).html('<input type="text" class="editBox" value="'+ text +'">\
-                <a href="#" class="save">save</a>');
+                <a href="#" class="save"><i class="fas fa-save"></i></a>');
         };
         // сохранение изменений 
         $('body').on('click', '.save', function() {
