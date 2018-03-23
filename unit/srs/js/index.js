d9ca494fa72592e9e75a38cdb563c12856f73aa4
Vladislav <vladislav08011993@outlook.com>
2018-03-23 08:20:33 +0300

    fff

--- unit/srs/js/index.js
+++ unit/srs/js/index.js
@@ -16,15 +16,15 @@ $(document).ready(function(){
             success: function (obj) {
                 $.each(obj, function (key, value) {
                     $('#out').append('<ul class="list"><button class="close_card"><i class="fas fa-times"></i></button>\
-                        <li class="name">'+'<span><i class="fa fa-user"></i> </span>'+ value.name +'</li>\
-                        <li class="email">' + '<span><i class="fa fa-envelope"></i> </span>' + value.email +'</li>\
-                        <li class="phone">' + '<span><i class="fa fa-mobile"></i> </span>' + value.phone +'</li>\
+                        <li class="name edit">' + '<span><i class="fas fa-user"></i> </span>' + value.name +'</li>\
+                        <li class="email edit">' + '<span><i class="fa fa-envelope"></i> </span>' + value.email +'</li>\
+                        <li class="phone edit">' + '<span><i class="fa fa-mobile"></i> </span>' + value.phone +'</li>\
                         <li class="adress">' + '<span>Adress: </span>' + '<ul>\
-                            <li class="place">' + '<span>street: </span>' + value.address.street + '</li>\
-                            <li class="place">' + '<span>suite: </span>' + value.address.suite + '</li>\
-                            <li class="place">' + '<span>city: </span>' + value.address.city + '</li>\
+                            <li class="place edit">' + '<span>street: </span>' + value.address.street + '</li>\
+                            <li class="place edit">' + '<span>suite: </span>' + value.address.suite + '</li>\
+                            <li class="place edit">' + '<span>city: </span>' + value.address.city + '</li>\
                             </ul>'+'</li>\
-                        <li class="company">' + '<span>Company: </span>' + value.company.name +'</li>\
+                        <li class="company edit">' + '<span>Company: </span>' + value.company.name +'</li>\
                     </ul>');
                 });
                 //удаляем кнопку из DOM
@@ -35,5 +35,20 @@ $(document).ready(function(){
         $('body').on('click', '.close_card', function(){
             $(this).parents('.list').remove();
         });
+
+        // работа с инфой
+        $('body').on('dblclick', '.edit', replaceHTML); 
+        function replaceHTML(){
+            var text =  $(this).text(); 
+
+            $(this).html('<input type="text" class="editBox" value="'+ text +'">\
+                <a href="#" class="save">save</a>');
+        };
+        // сохранение изменений 
+        $('body').on('click', '.save', function() {
+            var newText = $(this).siblings('.editBox').val();
+
+            $(this).parent().text(newText);
+        }); 
     });
 });
