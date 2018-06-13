687ffdbeedf56ed2173bef283048c5c2b968779a
Vladislav <vladislav08011993@outlook.com>
2018-06-13 21:21:15 +0300

    working good

--- unit/srs/js/app.js
+++ unit/srs/js/app.js
@@ -5,11 +5,12 @@ window.onload = function () {
   // создание кнопки для запроса данных
   var buttonGetData = document.createElement('button');
   buttonGetData.id = 'action';
+  buttonGetData.class = 'rigth';
   buttonGetData.classList = 'btn-small pulse blue'
   buttonGetData.appendChild((document.createTextNode('Get Data')));
 
   function appendBtn() {
-    document.querySelector('.staff').appendChild(buttonGetData);
+    document.querySelector('.getData').appendChild(buttonGetData);
     document.getElementById('action').addEventListener('click', getData);
   }
   //  проверка данных в localstorge
@@ -48,65 +49,65 @@ window.onload = function () {
     // компиляция HTML
     myData.forEach(function (value) {
       output += '<ul class="col s12 m5 card collection" id =' + value.id + '>\
-        <li class="collection-item">' + '<i class="fa fa-user"></i>' + '<input type="text" class="edit" id="name" value="' + value.name + '" disabled></li>\
-        <li class="collection-item">' + '<i class="fa fa-envelope">' + '</i><input class="edit" type="text" id="email"  value="' + value.email + '" disabled></li>\
-        <li class="collection-item">' + '<i class="fa fa-phone"></i>' + '<input class="edit" type="text" id="phone"  value="' + value.phone + '" disabled></li>\
+        <li class="collection-item">' + '<i class="fa fa-user"></i>' + '<span class="edit"> ' + value.name + '</span></li>\
+        <li class="collection-item">' + '<i class="fa fa-envelope">' + '</i><span class="edit"> ' + value.email + '</span></li>\
+        <li class="collection-item">' + '<i class="fa fa-phone"></i>' + '<span class="edit"> ' + value.phone + '</span></li>\
         <li class="address collection-item">' + '<strong>Adress</strong>' + '<ul>\
-          <li class="collection-item">' + '<span>street: </span>' + '<input class="edit" type="text" id="street" value = "' + value.address.street + '" disabled></li>\
-          <li class="collection-item">' + '<span>suite: </span>' + '<input class="edit" type="text" id="suite" value="' + value.address.suite + '" disabled></li>\
-          <li class="collection-item">' + '<span>city: </span>' + '<input class="edit" type="text" id="city" value="' + value.address.city + '" disabled></li>\
+          <li class="collection-item">' + '<span>street: </span>' + '<span class="edit"> ' + value.address.street + '</span></li>\
+          <li class="collection-item">' + '<span>suite: </span>' + '<span class="edit"> ' + value.address.suite + '</span></li>\
+          <li class="collection-item">' + '<span>city: </span>' + '<span class="edit" > ' + value.address.city + '</span></li>\
           </ul>'+ '</li>\
-        <li class="collection-item">' + '<strong>Company: </strong>' + '<input class="edit" type="text" id="company" value="' + value.company.name + '" disabled></li>\
-        <button class="delete-btn waves-effect waves-light btn-small">Delete</button>\
-        <button class="update-btn waves-effect waves-darken btn-small">Update</button>\
-        <!--<button class="save-btn waves-effect waves-darken btn-small">Save</button>-->\
+        <li class="collection-item">' + '<strong>Company</strong>' + '<span class="edit"> ' + value.company.name + '</span></li>\
+        <button class="delete-btn waves-effect waves-light btn-small red">Delete</button>\
+        <button class="update-btn waves-effect waves-darken btn-small orange">Update</button>\
         </ul>'
     });
-    document.querySelector('.staff').innerHTML = output;
+    document.querySelector('#staff').innerHTML = output;
   }
-
-  // удаление карточки из DOM 
+  // удаление карточки из DOM и LocalStorage
   document.body.addEventListener('click', removeList);
   function removeList(e) {
-    var idCard = e.target.parentElement;
+    var idCard = e.target.parentElement.id;
+    // idCard в число
+    var currentId = parseInt(idCard);
     // проверка условия по таргет
     if (e.target.classList.contains('delete-btn')) {
       e.target.parentElement.remove(); // удаление из DOM
       // удалние объекта из массива
-      for (var i = 0; i <= myData.length; i++)
-        if (myData[i].id == idCard.id) {
-          myData.splice(i, 1);
-          break;
-        }
+      Storage.deleteItemFromStorage(currentId);
       // проверка массива
       if (myData.length == 0) {
+
         appendBtn();
       }
     }
-    localStorage.setItem('card', JSON.stringify(myData)); // данные в localstorage
   }
-  const StorageCtrl = (function(){
-    // Public methods
+  // управление LocalStorage
+  var Storage = (function () {
     return {
-      updateItemStorage: function(updatedItem){
-        let items = JSON.parse(localStorage.getItem('card'));
-  
-        items.forEach(function(item, index){
-          if(updatedItem.id === item.id){
-            items.splice(index, 1, updatedItem);
+      updateItemStorage: function (updatedItem) {
+        myData.forEach(function (item, index) {
+          if (updatedItem.id === item.id) {
+            myData.splice(index, 1, updatedItem);
           }
         });
-        localStorage.setItem('card', JSON.stringify(items));
+        localStorage.setItem('card', JSON.stringify(myData));
+      },
+      deleteItemFromStorage: function (id) {
+        myData.forEach(function (item, index) {
+          if (id === item.id) {
+            myData.splice(index, 1);
+          }
+        });
+        localStorage.setItem('card', JSON.stringify(myData));
       },
     }
   })();
-  // update click
-  document.body.addEventListener('click', itemEditClick);
-  // item control
-  const ItemCtrl = (function () {
+  // Работа с данными пользователя
+  var ItemControl = (function () {
     return {
       getItemById: function (id) {
-        let found = null;
+        var found = null;
         myData.forEach(function (item) {
           if (item.id === id) {
             found = item;
@@ -114,13 +115,17 @@ window.onload = function () {
         });
         return found;
       },
-      updateItem: function (name, email) {
-        let found = null;
-
+      updateItem: function (name, email, phone, street, suite, city, job) {
+        var found = null;
         myData.forEach(function (item) {
           if (item.id === myData.currentItem.id) {
             item.name = name;
             item.email = email;
+            item.phone = phone;
+            item.address.street = street;
+            item.address.suite = suite;
+            item.address.city = city;
+            item.company.name = job;
             found = item;
           }
         });
@@ -134,94 +139,96 @@ window.onload = function () {
       },
     }
   })();
-  // ui control 
-  const UICtrl = (function () {
-    const UISelectors = {
+  // Работа с интерфейсом пользователя
+  var UI = (function () {
+    var UISelectors = {
       itemNameInput: '#item-name',
       itemEmailInput: '#item-email',
-      // itemPhoneInput: '#item-phone',
-      // itempStreerInput: '#item-street',
-      // itemSuiteInput: '#item-suite',
-      // itemCityInput: '#item-city',
+      itemPhoneInput: '#item-phone',
+      itemStreetInput: '#item-street',
+      itemSuiteInput: '#item-suite',
+      itemCityInput: '#item-city',
+      itemCompanyInput: '#item-company',
+      listItems: '#staff ul'
     }
     return {
       getItemInput: function () {
         return {
           name: document.querySelector(UISelectors.itemNameInput).value,
-          email: document.querySelector(UISelectors.itemEmailInput).value
+          email: document.querySelector(UISelectors.itemEmailInput).value,
+          phone: document.querySelector(UISelectors.itemPhoneInput).value,
+          street: document.querySelector(UISelectors.itemStreetInput).value,
+          suite: document.querySelector(UISelectors.itemSuiteInput).value,
+          city: document.querySelector(UISelectors.itemCityInput).value,
+          company: document.querySelector(UISelectors.itemCompanyInput).value
         }
       },
       addItemToForm: function () {
-        document.querySelector(UISelectors.itemNameInput).value = ItemCtrl.getCurrentItem().name;
-        document.querySelector(UISelectors.itemEmailInput).value = ItemCtrl.getCurrentItem().email;
+        document.querySelector(UISelectors.itemNameInput).value = ItemControl.getCurrentItem().name;
+        document.querySelector(UISelectors.itemEmailInput).value = ItemControl.getCurrentItem().email;
+        document.querySelector(UISelectors.itemPhoneInput).value = ItemControl.getCurrentItem().phone;
+        document.querySelector(UISelectors.itemStreetInput).value = ItemControl.getCurrentItem().address.street;
+        document.querySelector(UISelectors.itemSuiteInput).value = ItemControl.getCurrentItem().address.suite;
+        document.querySelector(UISelectors.itemCityInput).value = ItemControl.getCurrentItem().address.city;
+        document.querySelector(UISelectors.itemCompanyInput).value = ItemControl.getCurrentItem().company.name;
       },
       getSelectors: function () {
         return UISelectors;
       },
-      updateListItem: function(item){
-        let listItems = document.querySelectorAll(UISelectors.listItems);
-        // Turn Node list into array
-        listItems = Array.from(listItems);
-  
-        listItems.forEach(function(listItem){
-          const itemID = listItem.getAttribute('id');
-  
-          if(itemID === `item-${item.id}`){
-            document.querySelector(`#${itemID}`).innerHTML = `<strong>${item.name}: </strong> <em>${item.calories} Calories</em>
-            <a href="#" class="secondary-content">
-              <i class="edit-item fa fa-pencil"></i>
-            </a>`;
+      updateListItem: function (item) {
+        var listItems = document.querySelectorAll(UISelectors.listItems);
+        listItems.forEach(function (listItem) {
+          var itemID = listItem.getAttribute('id');
+          var currentItemId = parseInt(itemID);
+          if (currentItemId === item.id) {
+            setDataOnPage(myData);
           }
         });
       },
+      clearEditState: function () {
+        document.querySelector(UISelectors.itemNameInput).value = '';
+        document.querySelector(UISelectors.itemEmailInput).value = '';
+        document.querySelector(UISelectors.itemPhoneInput).value = '';
+        document.querySelector(UISelectors.itemStreetInput).value = '';
+        document.querySelector(UISelectors.itemSuiteInput).value = '';
+        document.querySelector(UISelectors.itemCityInput).value = '';
+        document.querySelector(UISelectors.itemCompanyInput).value = '';
+      },
     }
   })();
+
+  // Обновление данных пользователя
+  document.body.addEventListener('click', itemEditClick);
   function itemEditClick(e) {
     if (e.target.classList.contains('update-btn')) {
       // получение id card элемента
       var cardId = e.target.parentElement.id;
       // из строки в число
       var id = parseInt(cardId);
-
       // получение элемента
-      const itemToEdit = ItemCtrl.getItemById(id);
-      // Get item
-      console.log(itemToEdit)
-      ItemCtrl.setCurrentItem(itemToEdit);
-      // Add item to form
-      UICtrl.addItemToForm();
+      var itemToEdit = ItemControl.getItemById(id);
+      ItemControl.setCurrentItem(itemToEdit);
+      // Отправляем карточку в форму
+      UI.addItemToForm();
       e.preventDefault();
     }
   }
-  // Update item submit
+  // Отмена на изменение данных
+  document.body.querySelector('.cancel-btn').addEventListener('click', UI.clearEditState);
+  // Сохранение данных
   document.body.addEventListener('click', itemUpdateSubmit);
-   function itemUpdateSubmit(e) {
+  function itemUpdateSubmit(e) {
     if (e.target.classList.contains('save-btn')) {
-      console.log('test')
-      // Get item input
-      const input = UICtrl.getItemInput();
-
-      // Update item
-      const updatedItem = ItemCtrl.updateItem(input.name, input.email);
-
-      // Update UI
-      // UICtrl.updateListItem(updatedItem);
-
-      // Update local storage
-      // function updateItem(name, email) {
-      //   let items = JSON.parse(localStorage.getItem('card'));
-  
-      //   items.forEach(function(item, index){
-      //     if(updatedItem.id === item.id){
-      //       items.splice(index, 1, updatedItem);
-      //     }
-      //   });
-      //   localStorage.setItem('card', JSON.stringify(items));
-      // }
-      StorageCtrl.updateItemStorage(updatedItem);
-
-      // UICtrl.clearEditState();
-
+      // Получение профиля из формы
+      var input = UI.getItemInput();
+      // Обновление профиля
+      var updatedItem = ItemControl.updateItem(input.name, input.email, input.phone, input.street, input.suite, input.city, input.company);
+      // Обновление на странице
+      UI.updateListItem(updatedItem);
+      // Обновление в LocalStorage
+      Storage.updateItemStorage(updatedItem);
+      // Очистка формы после сохранения
+      UI.clearEditState();
       e.preventDefault();
     }
   }
